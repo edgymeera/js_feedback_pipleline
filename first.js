1695040c56d99d6767107c1901db523668bad21e
edgymeera <meerawork1352@outlook.com>
2023-05-16 10:57:20 +0400

    first

--- first.js
+++ first.js
@@ -1,1 +1,16 @@
-console.log("This is log")
+// console.log("This is log");
+
+// console.log(typeof numberOfStudents); //undefined
+// var numberOfStudents = 25;
+// console.log(typeof numberOfStudents); //number
+// numberOfStudents = 99;
+// console.log(typeof (typeof numberOfStudents)); //string
+// numberOfStudents = "nineteen"
+// console.log(typeof numberOfStudents); //string
+
+var1 = 23;
+console.log(typeof var1); 
+var2 = "hello";
+console.log(typeof var2); 
+var3 = null;
+console.log(typeof var3);
