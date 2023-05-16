c14eab9ebf8fc7c8443a505cde6e49b51516226a
edgymeera <meerawork1352@outlook.com>
2023-05-16 12:26:02 +0400

    first

--- first.js
+++ first.js
@@ -8,9 +8,24 @@
 // numberOfStudents = "nineteen"
 // console.log(typeof numberOfStudents); //string
 
-var1 = 23;
-console.log(typeof var1); 
-var2 = "hello";
-console.log(typeof var2); 
-var3 = null;
-console.log(typeof var3);
+// var1 = 23;
+// console.log(typeof var1); 
+// var2 = "hello";
+// console.log(typeof var2); 
+// var3 = null;
+// console.log(typeof var3);
+
+// myName = "Meera";
+// myAge = "23";
+// console.log(myName, myAge);
+// console.log(myName.indexOf("A"));
+// console.log(myName[0]);
+// console.log(myName.charAt(3));
+// console.log(myName.indexOf("e"));
+// console.log(myName.lastIndexOf("e"));
+
+// longString = "hey there my name is meera";
+// console.log(longString.endsWith("there"));
+// console.log(longString.endsWith("meera"));
+// console.log(longString.length);
+// console.log(longString.includes("meera"));
